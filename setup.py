33c7d7372b2ac678e40486365f545b9f99b2cfbc
VENKATESH TUNIKI <venkatajax@users.noreply.github.com>
2023-11-25 21:52:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/venkatajax/module",
-    packages=setuptools.find_packages(),
+    packages=['module'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License (GPL)",
